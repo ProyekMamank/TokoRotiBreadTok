759fd628c3f7befc2b0f548734955385b1e5116a
Nickolas Hartono <nickz.vict@gmail.com>
2021-05-09 23:30:41 +0700

    Added Insert Bahan to Admin

--- BreadTok/BreadTok/bahan.cs
+++ BreadTok/BreadTok/bahan.cs
@@ -18,7 +18,9 @@ namespace BreadTok
             dt.Columns.Add("MERK");
             dt.Columns.Add("JENIS BAHAN");
             dt.Columns.Add("STOCK");
+            dt.Columns.Add("HARGA");
             dt.Columns.Add("SUPPLIER");
+            dt.Columns.Add("ACTION");
 
             OracleCommand cmd = new OracleCommand();
             cmd.CommandText = "select * from bahan";
@@ -30,16 +32,20 @@ namespace BreadTok
                 dr[0] = reader.GetValue(1).ToString();
 
                 OracleCommand cmd2 = new OracleCommand();
-                cmd2.CommandText = $"select nama_jenis from jenis_bahan where ID = {Convert.ToInt32(reader.GetValue(3))}";
+                cmd2.CommandText = $"select nama_jenis from jenis_bahan where ID = {Convert.ToInt32(reader.GetValue(5))}";
                 cmd2.Connection = App.conn;
                 dr[1] = cmd2.ExecuteScalar().ToString();
 
-                dr[2] = reader.GetValue(2).ToString() + " gram";
+                dr[2] = reader.GetValue(2).ToString() + " " + reader.GetValue(4).ToString();
+
+                dr[3] = Convert.ToInt32(reader.GetValue(3));
 
                 OracleCommand cmd3 = new OracleCommand();
-                cmd3.CommandText = $"select nama from supplier where ID = {Convert.ToInt32(reader.GetValue(4))}";
+                cmd3.CommandText = $"select nama from supplier where ID = {Convert.ToInt32(reader.GetValue(6))}";
                 cmd3.Connection = App.conn;
-                dr[3] = cmd3.ExecuteScalar().ToString();
+                dr[4] = cmd3.ExecuteScalar().ToString();
+
+                dr[5] = reader.GetValue(0).ToString();
                 dt.Rows.Add(dr);
             }
             reader.Close();
