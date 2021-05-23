879585c1151f57ed7ba1348523c8bca97cb8b535
Yosua77 <mikhaelskho@gmail.com>
2021-05-24 03:04:29 +0700

    Revert "Added 2 Features(Pengadaan Bahan & Chef)"
    This reverts commit e7a7c22ce49377bcb86417a2436b80bf757892ce.

--- BreadTok/BreadTok/CurrencyConverter.cs
+++ BreadTok/BreadTok/CurrencyConverter.cs
@@ -1,33 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BreadTok
 {
-    static class CurrencyConverter
+    class CurrencyConverter
     {
-        public static string ToRupiah(this int angka)
-        {
-            return String.Format(CultureInfo.CreateSpecificCulture("id-id"), "Rp. {0:N}", angka);
-        }
-        /**
-         * // Usage example: //
-         * int angka = 10000000;
-         * System.Console.WriteLine(angka.ToRupiah()); // -> Rp. 10.000.000
-         */
-
-        public static int ToAngka(this string rupiah)
-        {
-            return int.Parse(Regex.Replace(rupiah, @",.*|\D", ""));
-        }
-        /**
-         * // Usage example: //
-         * string rupiah = "Rp 10.000.123,00";
-         * System.Console.WriteLine(rupiah.ToAngka()); // -> 10000123
-         */
     }
 }
