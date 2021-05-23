e7a7c22ce49377bcb86417a2436b80bf757892ce
Yosua77 <mikhaelskho@gmail.com>
2021-05-24 03:04:12 +0700

    Added 2 Features(Pengadaan Bahan & Chef)

---
+++ BreadTok/BreadTok/Supplier.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Oracle.DataAccess.Client;
+
+namespace BreadTok
+{
+    class Supplier
+    {
+        public string getColumnByID(string columnName, int id)
+        {
+            OracleCommand cmd = new OracleCommand($"SELECT {columnName.ToUpper()} FROM SUPPLIER WHERE ID = {id}", App.conn);
+            return cmd.ExecuteScalar().ToString();
+        }
+        public string getColumnByKode(string columnName, string kode)
+        {
+            OracleCommand cmd = new OracleCommand($"SELECT {columnName.ToUpper()} FROM SUPPLIER WHERE KODE = '{kode}'", App.conn);
+            return cmd.ExecuteScalar().ToString();
+        }
+
+        public DataTable GetDataTable(string columns)
+        {
+            DataTable listSupplier = new DataTable();
+            OracleCommand cmd = new OracleCommand($"SELECT {columns} FROM SUPPLIER", App.conn);
+            cmd.ExecuteReader();
+            OracleDataAdapter adapter = new OracleDataAdapter(cmd);
+            adapter.Fill(listSupplier);
+            return listSupplier;
+        }
+    }
+}
