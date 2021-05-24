a6fcd42520fc7edcd5ae7b955e8477096667464e
Lawrence <lawrencepatrick0@gmail.com>
2021-05-24 11:46:38 +0700

    Update Database on Main

--- query.sql
+++ query.sql
@@ -12,31 +12,57 @@ END;
 SHOW ERR;
 
 CREATE OR REPLACE TRIGGER AUTOGEN_ID_BAHAN
-BEFORE INSERT ON BAHAN
+BEFORE INSERT OR UPDATE ON BAHAN
 FOR EACH ROW
 DECLARE
     ID varchar2(100);
+	CHECK0 varchar2(20);
+	CHECK1 varchar2(20);
 	START_CODE varchar2(20);
     END_CODE varchar2(20);
+	flag number(10);
+	pragma autonomous_transaction;
 BEGIN
-    select to_char(max(to_number(ID))+1)
-    into ID
-    from BAHAN;
+	flag := 0;
+	if updating then
+		IF INSTR(:OLD.MERK, ' ') > 0 THEN
+			CHECK0 := SUBSTR(:OLD.MERK, 1, 2) || SUBSTR(:OLD.MERK, INSTR(:OLD.MERK, ' ') + 1, 2);
+			CHECK1 := SUBSTR(:NEW.MERK, 1, 2) || SUBSTR(:NEW.MERK, INSTR(:NEW.MERK, ' ') + 1, 2);
+		ELSE
+			CHECK0 := SUBSTR(:OLD.MERK, 1, 4);
+			CHECK1 := SUBSTR(:NEW.MERK, 1, 4);
+		END IF;
+		
+		if CHECK0 <> CHECK1 then
+			flag := 2;
+		end if;
+	elsif inserting then
+		flag := 1;
+	end if;
 	
-	IF INSTR(:NEW.MERK, ' ') > 0 THEN
-        START_CODE := SUBSTR(:NEW.MERK, 1, 2) || SUBSTR(:NEW.MERK, INSTR(:NEW.MERK, ' ') + 1, 2);
-    ELSE
-        START_CODE := SUBSTR(:NEW.MERK, 1, 4);
-    END IF;
-	
-    SELECT LPAD(NVL(MAX(TO_NUMBER(SUBSTR(B.KODE, 5,5))), 0)+1, 5, '0') INTO END_CODE
-    FROM BAHAN B
-    WHERE SUBSTR(B.KODE, 1, 4) = UPPER(START_CODE);
+	if flag > 0 then
+		select to_char(max(to_number(ID))+1)
+		into ID
+		from BAHAN;
+		
+		IF INSTR(:NEW.MERK, ' ') > 0 THEN
+			START_CODE := SUBSTR(:NEW.MERK, 1, 2) || SUBSTR(:NEW.MERK, INSTR(:NEW.MERK, ' ') + 1, 2);
+		ELSE
+			START_CODE := SUBSTR(:NEW.MERK, 1, 4);
+		END IF;
+		
+		SELECT LPAD(NVL(MAX(TO_NUMBER(SUBSTR(B.KODE, 5,5))), 0)+1, 5, '0') INTO END_CODE
+		FROM BAHAN B
+		WHERE SUBSTR(B.KODE, 1, 4) = UPPER(START_CODE);
 
-    :NEW.KODE := START_CODE || END_CODE;
-	:NEW.PICTURE_LOCATION := START_CODE || END_CODE;
+		:NEW.KODE := START_CODE || END_CODE;
+		:NEW.PICTURE_LOCATION := START_CODE || END_CODE || '.jpg';
 
-    :new.ID := ID;
+		if flag = 1 then
+			:new.ID := ID;
+			:new.STATUS := 1;
+		end if;
+	end if;
 END;
 /
 
@@ -64,8 +90,45 @@ BEGIN
 
 
     :NEW.KODE := UPPER(START_CODE || END_CODE);
-	:NEW.PICTURE_LOCATION := UPPER(START_CODE || END_CODE);
+	:NEW.PICTURE_LOCATION := UPPER(START_CODE || END_CODE) ||  '.jpg';
 
     :new.ID := ID;
 END;
 /
+
+CREATE OR REPLACE TRIGGER UPDATE_ID_ROTI
+BEFORE UPDATE ON ROTI
+FOR EACH ROW
+DECLARE
+    ID varchar2(100);
+	START_CODE varchar2(20);
+    END_CODE varchar2(20);
+	pragma autonomous_transaction;
+BEGIN
+    IF INSTR(:NEW.NAMA, ' ') > 0 THEN
+        START_CODE := SUBSTR(:NEW.NAMA, 1, 2) || SUBSTR(:NEW.NAMA, INSTR(:NEW.NAMA, ' ') + 1, 2);
+    ELSE
+        START_CODE := SUBSTR(:NEW.NAMA, 1, 4);
+    END IF;
+
+    if(START_CODE != SUBSTR(:OLD.KODE,1,4)) then
+        select to_char(max(to_number(ID))+1)
+        into ID
+        from ROTI;
+
+        SELECT LPAD(NVL(MAX(TO_NUMBER(SUBSTR(R.KODE, 5,5))), 0)+1, 5, '0') INTO END_CODE
+        FROM ROTI R
+        WHERE SUBSTR(R.KODE, 1, 4) = UPPER(START_CODE);
+
+        :NEW.KODE := UPPER(START_CODE || END_CODE);
+        :NEW.PICTURE_LOCATION := UPPER(START_CODE || END_CODE) ||  '.jpg';
+    else
+        :NEW.KODE := :OLD.KODE;
+        :NEW.PICTURE_LOCATION := :OLD.KODE ||  '.jpg';
+    end if;
+END;
+/
+
+COMMIT;
+
+
